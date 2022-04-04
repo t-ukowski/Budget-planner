e56078a86366e27c36f631db432f9359e7902f80
Tomon <t.ukowski@gmail.com>
2022-04-04 14:54:28 +0200

    Sidebar data now properly connected

--- frontend/src/Components/SidebarData.js
+++ frontend/src/Components/SidebarData.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import CalculateIcon from '@mui/icons-material/Calculate';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 export const SidebarData = [
     {
-        title: "raz",
-        icon: <Icon />,
+        title: "Raz",
+        icon: <HomeIcon />,
         link: "/home"
+    },
+    {
+        title: "Dwa",
+        icon: <CalendarMonthIcon />,
+        link: "/history"
+    },
+    {
+        title: "Trzy",
+        icon: <CalculateIcon />,
+        link: "/objectives"
+    },
+    {
+        title: "Cztery",
+        icon: <SettingsIcon />,
+        link: "/settings"
     }
 ]
