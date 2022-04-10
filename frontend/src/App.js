021d5c4284795be765cf9a015dd0a7957e65ccec
Tomon <t.ukowski@gmail.com>
2022-04-10 17:45:03 +0200

    Balanced, Title components, additional styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import './styles/main.scss';
-import Sidebar from './Components/Sidebar';
+import Sidebar from './components/Sidebar';
+import Balance from './components/scoreboards/Balance';
+import Title from './components/Title';
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar />
+    <div className="app">
+      <aside>
+        <Sidebar />
+      </aside>
+      <main>
+        <Title text="Budget planner" />
+        <Balance />
+      </main>
     </div>
   );
 }
---
+++ frontend/src/Components/Title.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Title(text) {
+  return <div className="title">{text.text}</div>;
+}
+
+export default Title;
