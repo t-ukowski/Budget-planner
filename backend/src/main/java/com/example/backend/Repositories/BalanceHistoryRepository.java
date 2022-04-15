1e2556a788055007f9e7fd9b478d388060a407d1
Bartłomiej <kiszka2200@onet.pl>
2022-04-15 23:54:00 +0200

    First draft of BankAccount CRUD v2

--- backend/src/main/java/com/example/backend/Repositories/BalanceHistoryRepository.java
+++ backend/src/main/java/com/example/backend/Repositories/BalanceHistoryRepository.java
@@ -2,6 +2,10 @@ package com.example.backend.Repositories;
 
 import com.example.backend.model.BalanceHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BalanceHistoryRepository extends JpaRepository<BalanceHistory, Long> {
+
 }
