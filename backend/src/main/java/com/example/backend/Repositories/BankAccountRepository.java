a0a7564689242795a2e3d964f50583eb7318bf2e
lstachon <64076091+lstachon@users.noreply.github.com>
2022-04-17 23:02:57 +0200

    not used function deleted

--- backend/src/main/java/com/example/backend/Repositories/BankAccountRepository.java
+++ backend/src/main/java/com/example/backend/Repositories/BankAccountRepository.java
@@ -10,5 +10,4 @@ import java.util.List;
 public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
     @Query("select b from BankAccount b where b.user = ?1 and b.accountName = ?2")
     List<BankAccount> findBankAccountsByUserAndAccountName(User user, String accountName);
-
 }
