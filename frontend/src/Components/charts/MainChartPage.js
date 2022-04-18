da6c42c155bc9f4b2dc6eb469a1da320427b04b8
Tomon <t.ukowski@gmail.com>
2022-04-18 19:04:36 +0200

    React Hooks added to support chart refreshing

--- frontend/src/Components/charts/MainChartPage.js
+++ frontend/src/Components/charts/MainChartPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   ResponsiveContainer,
   CartesianGrid,
@@ -11,36 +11,43 @@ import {
 import Title from '../page/Title';
 
 function MainChartPage() {
-  const data = [
-    {
-      date: '12-20-2020',
-      balance: 10202
-    },
-    {
-      date: '13-20-2020',
-      balance: 12202
-    },
-    {
-      date: '13-20-2020',
-      balance: 9002
-    },
-    {
-      date: '15-20-2020',
-      balance: 8202
-    },
-    {
-      date: '20-20-2020',
-      balance: 14202
-    },
-    {
-      date: '23-20-2020',
-      balance: 17202
-    },
-    {
-      date: '25-20-2020',
-      balance: 13202
-    }
-  ];
+  const [page, setPage] = useState(0);
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    console.log('Cart reloading');
+    setChartData([
+      {
+        date: '12-20-2020',
+        balance: 10202
+      },
+      {
+        date: '13-20-2020',
+        balance: 12202
+      },
+      {
+        date: '13-20-2020',
+        balance: 9002
+      },
+      {
+        date: '15-20-2020',
+        balance: 8202
+      },
+      {
+        date: '20-20-2020',
+        balance: 14202
+      },
+      {
+        date: '23-20-2020',
+        balance: 17202
+      },
+      {
+        date: '25-20-2020',
+        balance: 13202
+      }
+    ]);
+  }, [page]);
+
   return (
     <div className="flexCard">
       <div className="mainCard">
@@ -49,8 +56,11 @@ function MainChartPage() {
         </div>
         <div className="chartCard">
           <div className="chartCardInside">
+            <button disabled={page <= 0 ? true : false} onClick={() => setPage(page - 1)}>
+              ❮
+            </button>
             <ResponsiveContainer width="95%" height="95%">
-              <LineChart data={data}>
+              <LineChart data={chartData}>
                 <Line type="monotone" dataKey="balance" stroke="#003049" strokeWidth={3} />
                 <CartesianGrid strokeDasharray="3 3" vertical={false} />
                 <XAxis dataKey="date" />
@@ -58,6 +68,7 @@ function MainChartPage() {
                 <Tooltip />
               </LineChart>
             </ResponsiveContainer>
+            <button onClick={() => setPage(page + 1)}>❯</button>
           </div>
         </div>
       </div>
