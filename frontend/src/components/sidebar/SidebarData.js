7a494632bd94247fca6ed4d0e071d0a3a53b3ffc
Tomon <t.ukowski@gmail.com>
2022-04-20 13:55:23 +0200

    Emergency fix

---
+++ frontend/src/components/sidebar/SidebarData.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import PieChartIcon from '@mui/icons-material/PieChart';
+import SsidChartIcon from '@mui/icons-material/SsidChart';
+import FlagIcon from '@mui/icons-material/Flag';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+export const SidebarData = [
+  {
+    title: 'Strona' + '\xa0' + 'główna',
+    icon: <HomeIcon />,
+    link: 'home'
+  },
+  {
+    title: 'Saldo',
+    icon: <PieChartIcon />,
+    link: 'account'
+  },
+  {
+    title: 'Cashflow',
+    icon: <SsidChartIcon />,
+    link: 'cashflow'
+  },
+  {
+    title: 'Cele',
+    icon: <FlagIcon />,
+    link: 'objectives'
+  },
+  {
+    title: 'Ustawienia',
+    icon: <SettingsIcon />,
+    link: 'settings'
+  }
+];
