e0fa5d5cddd035deb42ce2df09c84d03be0ca16d
Ehevi <aga.cyra@wp.pl>
2022-05-10 16:07:16 +0200

    deleting main goals :candy:

--- frontend/src/components/goals/CheckBoxes.js
+++ frontend/src/components/goals/CheckBoxes.js
@@ -4,12 +4,15 @@ import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import AddSubgoalModal from '../modals/AddSubgoalModal';
 import AddIcon from '@mui/icons-material/AddBox';
+import DeleteIcon from '@mui/icons-material/Delete';
+import axios from 'axios';
 import { Button } from '@mui/material';
 
 export default function CheckBoxes({ parentGoal }) {
   const [modalIsOpen, setIsOpen] = useState(false);
   const [subgoals, setSubgoals] = useState(parentGoal.subgoals);
   const [parent, setParent] = useState(parentGoal.name);
+  const [deleted, setDeleted] = useState(false);
 
   function openModal() {
     setIsOpen(true);
@@ -28,6 +31,16 @@ export default function CheckBoxes({ parentGoal }) {
       });
   }, [modalIsOpen]);
 
+  function handleDelete() {
+    axios({
+      method: 'delete',
+      url: `http://localhost:8080/goals/${parentGoal.id}`
+    })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err.data));
+    setDeleted(true);
+  }
+
   const children = (
     <Box sx={{ display: 'flex', flexDirection: 'column', ml: 3 }}>
       {subgoals.map((subgoal) => {
@@ -43,16 +56,25 @@ export default function CheckBoxes({ parentGoal }) {
   );
 
   return (
-    <div>
-      <FormControlLabel
-        label={parent}
-        control={<Checkbox checked={!parentGoal.subgoals.map((s) => s.achieved).includes(false)} />}
-      />
-      {children}
-      <Button className="addButton" onClick={openModal}>
-        <AddIcon className="icon" />
-      </Button>
-      <AddSubgoalModal modalIsOpen={modalIsOpen} closeModal={closeModal} parent={parent} />
-    </div>
+    <>
+      {!deleted && (
+        <div>
+          <FormControlLabel
+            label={parent}
+            control={
+              <Checkbox checked={!parentGoal.subgoals.map((s) => s.achieved).includes(false)} />
+            }
+          />
+          <Button className="addButton" onClick={handleDelete}>
+            <DeleteIcon className="icon" />
+          </Button>
+          {children}
+          <Button className="addButton" onClick={openModal}>
+            <AddIcon className="icon" />
+          </Button>
+          <AddSubgoalModal modalIsOpen={modalIsOpen} closeModal={closeModal} parent={parent} />
+        </div>
+      )}
+    </>
   );
 }
