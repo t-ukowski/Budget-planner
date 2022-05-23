9d87989d9a67262e95cd76d1ad0185f6858b9c75
Bartłomiej <kiszka2200@onet.pl>
2022-05-23 17:06:19 +0200

    fix

--- backend/src/main/java/com/example/backend/controller/IncomesExpensesController.java
+++ backend/src/main/java/com/example/backend/controller/IncomesExpensesController.java
@@ -64,6 +64,7 @@ public class IncomesExpensesController {
         List<BankAccount> userAccounts = bankAccountRepository.findBankAccountsByUserAndAccountName(user, accountName);
         BankAccount bankAccount = userAccounts.get(0);
         currentAction.setAccountName(accountName);
+        currentAction.setPaid(false);
         currentAction.setBankAccount(bankAccount);
         currentAction.setBillingDate(Date.valueOf(billingDate));
         currentAction.setEndBillingDate(Date.valueOf(endBillingDate));
