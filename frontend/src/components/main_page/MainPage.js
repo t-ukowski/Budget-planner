22b02e1e458bad3fef3d3b2cea806592c6a5efb7
Tomon <t.ukowski@gmail.com>
2022-06-07 20:57:09 +0200

    Contents of main page

--- frontend/src/components/main_page/MainPage.js
+++ frontend/src/components/main_page/MainPage.js
@@ -3,11 +3,96 @@ import MoreButton from './MoreButton';
 
 function MainPage({ updateNeeded }) {
   const [balanceData, setBalanceData] = useState(0);
+  const [expensePanel, setExpensePanel] = useState([]);
+  const [incomePanel, setIncomePanel] = useState([]);
+  const [goalsPanel, setGoalsPanel] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:8080/TotalBalance')
       .then((res) => res.json())
       .then((json) => setBalanceData(json));
+    fetch(`http://localhost:8080/BalanceOperations?page=0&chartInterval=60`)
+      .then((res2) => res2.json())
+      .then((balanceOperations) => {
+        balanceOperations.sort(function (a, b) {
+          if (a.billingDate < b.billingDate) {
+            return -1;
+          }
+          if (a.billingDate > b.billingDate) {
+            return 1;
+          }
+          return 0;
+        });
+
+        let counter = 0;
+        var i = 78067;
+        var tempExpensePanel = [];
+        for (let operation of balanceOperations) {
+          if (operation.type === 'Wydatek') {
+            tempExpensePanel.push(
+              <li key={i}>
+                <b>{operation.description}</b>
+              </li>
+            );
+            tempExpensePanel.push(<li key={i + 1}>{operation.billingDate}</li>);
+            tempExpensePanel.push(
+              <li key={i + 2}>
+                <b>{operation.amount}zł</b>
+              </li>
+            );
+            counter++;
+          }
+          i += 3;
+          if (counter === 3) break;
+        }
+        setExpensePanel(<ul className="incomes_list">{tempExpensePanel}</ul>);
+        counter = 0;
+        var tempIncomePanel = [];
+        for (let operation of balanceOperations) {
+          if (operation.type !== 'Wydatek') {
+            tempIncomePanel.push(
+              <li key={i}>
+                <b>{operation.description}</b>
+              </li>
+            );
+            tempIncomePanel.push(<li key={i + 1}>{operation.billingDate}</li>);
+            tempIncomePanel.push(
+              <li key={i + 2}>
+                <b>{operation.amount}zł</b>
+              </li>
+            );
+            counter++;
+          }
+          i += 3;
+          if (counter === 3) break;
+        }
+        setIncomePanel(<ul className="incomes_list">{tempIncomePanel}</ul>);
+      });
+    fetch('http://localhost:8080/GoalRealization')
+      .then((res) => res.json())
+      .then((goals) => {
+        var counter = 0;
+        var i = 780367;
+        var tempGoalsPanel = [];
+        for (let goal of goals) {
+          console.log(goal);
+          console.log(i);
+          tempGoalsPanel.push(
+            <li key={i}>
+              <b>{goal.goal.goalName}</b>
+            </li>
+          );
+          tempGoalsPanel.push(
+            <li key={i + 1}>
+              Najbliższa możliwa data realizacji: <b>{goal.date}</b>
+            </li>
+          );
+          counter++;
+          i += 2;
+          if (counter === 3) break;
+        }
+        setGoalsPanel(<ul className="goals_list">{tempGoalsPanel}</ul>);
+      });
   }, [updateNeeded]);
 
   return (
@@ -25,6 +110,7 @@ function MainPage({ updateNeeded }) {
         <div className="scoreboard small">
           <div className="content">
             <div className="heading2">Wydatki</div>
+            {expensePanel}
           </div>
           <div className="more_button_container">
             <MoreButton to="cashflow" />
@@ -33,6 +119,7 @@ function MainPage({ updateNeeded }) {
         <div className="scoreboard small">
           <div className="content">
             <div className="heading2">Przychody</div>
+            {incomePanel}
           </div>
           <div className="more_button_container">
             <MoreButton to="cashflow" />
@@ -42,6 +129,7 @@ function MainPage({ updateNeeded }) {
       <div className="scoreboard large">
         <div className="content">
           <div className="heading2">Cele</div>
+          {goalsPanel}
         </div>
         <div className="more_button_container">
           <MoreButton to="objectives" />
