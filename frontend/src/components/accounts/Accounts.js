7673de13d9e056bae128f573060773a247932934
Ehevi <aga.cyra@wp.pl>
2022-06-08 12:52:57 +0200

    fixing bugs :bug:

--- frontend/src/components/accounts/Accounts.js
+++ frontend/src/components/accounts/Accounts.js
@@ -21,18 +21,6 @@ export default function Accounts({ updateNeeded, setUpdateNeeded }) {
     setUpdateNeeded(!updateNeeded);
   }
 
-  /*
-  function addColor() {
-    const accountsWithColors = accountsList.map(({ id, accountBalance, accountName }, index) => ({
-      id: id,
-      accountBalance: accountBalance,
-      accountName: accountName,
-      color: piechartColors[index]
-    }));
-    setAccountsList(accountsWithColors);
-  }
-*/
-
   useEffect(() => {
     fetch('http://localhost:8080/AccountsList')
       .then((res) => res.json())
@@ -46,7 +34,6 @@ export default function Accounts({ updateNeeded, setUpdateNeeded }) {
           }))
         );
       });
-    // .then(addColor());
   }, [updateNeeded]);
 
   return (
@@ -56,7 +43,7 @@ export default function Accounts({ updateNeeded, setUpdateNeeded }) {
       <table>
         <tbody>
           <tr>
-            <th>Konto</th>
+            <th>Kolor</th>
             <th>Konto</th>
             <th>Saldo</th>
           </tr>
@@ -75,9 +62,15 @@ export default function Accounts({ updateNeeded, setUpdateNeeded }) {
       </table>
       <br />
       <br />
-      <Button className="iconButton small" sx={addButtonStyleSmall} onClick={openModal}>
-        <AddIcon className="icon" />
-      </Button>
+      {accountsList.length < 10 && (
+        <Button className="iconButton small" sx={addButtonStyleSmall} onClick={openModal}>
+          <AddIcon className="icon" />
+        </Button>
+      )}
+      {accountsList.length === 10 && (
+        <div className="text-base italic marg">Osiągnięto maksymalną liczbę kont</div>
+      )}
+
       <BalancePieChart />
     </>
   );
